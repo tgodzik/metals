bd9816b3ea29fcb4074cf7bcf7b63b3ee393783c
Krrish Biswas <krrishbiswas175@gmail.com>
2026-01-29 15:50:06 +0530

    fix: Recompile worksheet on changes even if didFocus is not supported (#8141)
    Fixes #7090
    
    ## Problem
    
    When the client (editor) does not support the `didFocus` capability,
    worksheets were not being evaluated/recompiled on save if another file
    had been opened after the worksheet. This happened because
    `onWorksheetChanged` checked `focusedDocument.contains(path)` even when
    `didFocus` was not supported.
    
    Without `didFocus` events, the `focusedDocument` tracking (which relies
    on `didOpen`) becomes stale when users switch tabs, causing worksheet
    evaluations to be skipped.
    
    ## Solution
    
    Modified the condition in `onWorksheetChanged` to only check
    `focusedDocument.contains(path)` when
    `clientConfig.isDidFocusProvider()` is true. If `didFocus` is not
    supported, any changed worksheet will be evaluated regardless of the
    current `focusedDocument` state.
    
    ## Changes
    
    -
    [MetalsLspService.scala](cci:7://file:///Users/krrishbiswas/Desktop/GSoC/metals/metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala:0:0-0:0):
    Removed `|| focusedDocument.isDefined` from the condition in
    `onWorksheetChanged`
    
    ## Testing
    
    Added a new test suite `Issue7090LspSuite` that:
    1. Disables `didFocusProvider`
    2. Opens a worksheet, then opens another file
    3. Modifies and saves the worksheet
    4. Asserts that the worksheet is evaluated (via diagnostics)
    
    (cherry picked from commit 1e5d1db08c6bf670662b7dd5695460b32701132c)

--- metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala
+++ metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala
@@ -2122,7 +2122,7 @@ abstract class MetalsLspService(
   ): Future[Unit] = {
     paths
       .find { path =>
-        if (clientConfig.isDidFocusProvider() || focusedDocument.isDefined) {
+        if (clientConfig.isDidFocusProvider()) {
           focusedDocument.contains(path) &&
           path.isWorksheet
         } else {
